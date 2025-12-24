2e66d302e82643de89d825ecbd5aac2b10b8155d
Rohini0518 <rohinipolina0518@gmail.com>
2025-12-24 21:24:36 +0530

    added frontend themeprovider for overall project

---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,12 @@
+import {createTheme} from '@mui/material/styles'
+
+export const theme=createTheme({
+    palette:{
+        primary:{
+            main:"#0000"
+        },
+        secondary:{
+            main:"#E91E63"
+        }
+    }
+})
