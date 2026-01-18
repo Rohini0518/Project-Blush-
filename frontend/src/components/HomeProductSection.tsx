56befa4babc4cfa4de00b3c9a186bb4c686b9f44
Rohini0518 <rohinipolina0518@gmail.com>
2026-01-18 13:03:41 +0530

    added dummy product data and grid products

--- frontend/src/components/HomeProductSection.tsx
+++ frontend/src/components/HomeProductSection.tsx
@@ -1,20 +1,67 @@
 import { Box, Typography } from "@mui/material";
 import ProductCard from "../cards/ProductCard";
+import { useEffect, useState } from "react";
 
-export default function HomeProductSection(){
-const img="https://images.unsplash.com/photo-1713296134277-6917ab5b752f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI1fHx3b21lbiUyMGluZGlhfGVufDB8fDB8fHww"    
-    return(<>
-    <Box sx={{display:"flex" ,flexDirection:"column",mt:10}}>
-        <Box sx={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-      <Typography variant="h2">sectionTitle</Typography>
-      <Typography variant="h4">sectionSubtitle</Typography>
-      <Typography sx={{color:"red"}}>(❁´◡`❁)</Typography>
+type product = {
+  image: string;
+  id: number;
+  price: number;
+  title: string;
+};
+export default function HomeProductSection() {
+  const [data, setData] = useState<product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://fakestoreapiserver.reactbd.org/api/products?page=1&perPage=10",
+        );
+        const datajson = await response.json();
+        setData(datajson.data);
+        console.log(datajson);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Box sx={{ display: "flex", flexDirection: "column", mt: 10 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h2">Fashion Wear</Typography>
+          <Typography variant="h4">sectionSubtitle</Typography>
+          <Typography sx={{ color: "red" }}>(❁´◡`❁)</Typography>
         </Box>
 
-        <Box sx={{m:"10px",width:"80%"}}>
-            <ProductCard img={img} productInfo="Pure Mul Cotton Zari angarakha Suit Set with best price " price={1500}/>
+        <Box
+          sx={{
+            m: "10px",
+            width: "90%",
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "20px",
+          }}
+        >
+          {data.map((product) => (
+            <ProductCard
+              key={product.id}
+              img={product.image}
+              productInfo={product.title}
+              price={product.price}
+            />
+          ))}
         </Box>
-
-    </Box>
-    </>)
+      </Box>
+    </>
+  );
 }
