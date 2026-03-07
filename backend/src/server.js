90e05db24f055b61391e0279e9637e3542a69225
Rohini0518 <rohinipolina0518@gmail.com>
2026-03-07 17:22:09 +0530

    all needed packages added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,31 @@
 import express from "express";
 import cors from 'cors';
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 const app=express();
 
-app.use(cors());
+//cookie parseris a middleware that translates browser cookies into redable javascript objects
+//cors is a middleware that controls who can talk to our api
+app.use(cors({
+    origin:'http://localhost:5173/',
+    methods:['GET','POST','DELETE','PUT'],
+    allowedHeaders:[
+        "content-Type","Authorization","Cache-Control","Expires","Pragma"
+    ],
+    credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/",(req,res)=>{
     res.send("Blush backedn running 🚀🚀");
 })
-
+ 
 app.listen(5000,()=>{
-    console.log("server running on prt 5000");
+    console.log("server running on port 5000");
 })
+mongoose.connect(` mongodb+srv://rohinipolina0518_db_user:rY2yNvs8NPeVQ7IO@clusterblush.1axaliw.mongodb.net/`)
+.then(()=>console.log('momgodb connected'))
+.catch(error=>console.log("error-",error))
+
+//  mongodb+srv://rohinipolina0518_db_user:<db_password>@clusterblush.1axaliw.mongodb.net/ 
